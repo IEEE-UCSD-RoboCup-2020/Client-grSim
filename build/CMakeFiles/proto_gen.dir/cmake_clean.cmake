2dae929124b19e95a3051e8bd1599498e9a901ba
Haoen Luo <samuelluohaoen@hotmail.com>
2020-07-25 05:18:13 -0700

    First commit. Currently client can only send packet using protobuf.

--- build/CMakeFiles/proto_gen.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/proto_gen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/proto_gen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/proto/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/samuelluohaoen/Code/RoboCup/Client-grSim/proto
-# Build directory: /home/samuelluohaoen/Code/RoboCup/Client-grSim/build/proto
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
