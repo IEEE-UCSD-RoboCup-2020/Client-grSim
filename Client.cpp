b91ab4dd2542bea4d420dc8501ce282c8b4f820f
Haoen Luo <samuelluohaoen@hotmail.com>
2020-07-25 20:10:35 -0700

    Now using vFirmware_API.proto

--- Client.cpp
+++ Client.cpp
@@ -3,12 +3,26 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/thread/thread.hpp>
-#include "proto/message.pb.h"
+#include "proto/vFirmware_API.pb.h"
 
 int main(int argc, char* argv[])
 {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
-    example::Message message;
+
+    VF_Commands commands;
+    Vec_2D trans_vec;
+    Vec_2D kick_vec;
+    float trans_vec_x = 0;
+    float trans_vec_y = 0;
+    float rotate = 0;
+    float kick_vec_x = 0;
+    float kick_vec_y = 0;
+    bool drib = false;
+    
+    // VF_Data data;
+
+    std::string line;
+    std::string output;
 
     unsigned int port = 0;
     std::string ip = "";
@@ -32,16 +46,63 @@ int main(int argc, char* argv[])
 
         std::cout << "Successsfully connected to " << ep.address() << " port " << ep.port() << std::endl;
 
-        std::string line;
-        std::string output;
+
+
 
         while(true)
         {
+            std::cout << "Initialize sensor systems? Y or N : ";
             std::cin >> line;
+            std::cout << std::endl;
+        
+            if(toupper(line.at(0)) == 'Y') 
+            {
+                commands.set_init(true);
+                break;
+            }
+            else
+            {
+                commands.set_init(false);
+                break;
+            }
+        }
 
-            message.set_mesg(line);
-            message.SerializeToString(&output);
+        while(true)
+        {
+            line = "";
+            output = "";
+            trans_vec.Clear();
+            kick_vec.Clear();
+            trans_vec_x = 0;
+            trans_vec_y = 0;
+            rotate = 0;
+            kick_vec_x = 0;
+            kick_vec_y = 0;
+            drib = false;
+
+
+
+            std::cout << "Input required: " << std::endl;
+            std::cout << "Format: <translational_output_vec: x y> <rotational_output> <kicker_vec: x y> <dribbler on? Y or N>:" 
+                << std::endl;
+            
+            std::cin >> trans_vec_x >> trans_vec_y >> rotate >> kick_vec_x >> kick_vec_y >> line;
+            
+            drib = toupper(line.at(0)) == 'Y'? 1 : 0;
+
+            trans_vec.set_x(trans_vec_x);
+            trans_vec.set_y(trans_vec_y);
+            kick_vec.set_x(kick_vec_x);
+            kick_vec.set_y(kick_vec_y);
+            commands.set_allocated_translational_output(&trans_vec);
+            commands.set_rotational_output(rotate);
+            commands.set_allocated_kicker(&kick_vec);
+            commands.set_dribbler(drib);
+            commands.SerializeToString(&output);
             socket.write_some(boost::asio::buffer(output));
+            std::cout << "Packet sent." << std::endl;
+            commands.release_kicker();
+            commands.release_translational_output();
         }
 
     }
